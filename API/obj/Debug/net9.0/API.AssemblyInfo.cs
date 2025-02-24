0ad7eb624a9f44a52ca825a3ae37481867f30ed2
Durlove <sainjudurlove@gmail.com>
2025-02-24 17:34:44 +0545

    more of the same

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f191e263d076739504e076bb2cb2706844e33fa6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf3dfd6fd23c4eb1713e16ce5b53a75a64ce860b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
