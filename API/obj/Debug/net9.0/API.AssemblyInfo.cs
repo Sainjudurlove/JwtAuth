78d61a49b24890a6e64d4e85b6032cc54ea4d0e0
Durlove <sainjudurlove@gmail.com>
2025-02-27 13:31:55 +0545

    Installed Tailwindcss in angular

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ad7eb624a9f44a52ca825a3ae37481867f30ed2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1d75daf96ec5d9185fb02cb06453fa8fc2ed0f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
